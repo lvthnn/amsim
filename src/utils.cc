e50284f314d62fbd9cdf8e914ea6756521b25c3e
Kári Hlynsson <karihlynsson@Modulor-3.local>
2025-10-05 20:57:10 +0000

    starting work on refactoring phenotype values into shared buffer

--- src/utils.cc
+++ src/utils.cc
@@ -1,5 +1,6 @@
 #include <cstddef>
 #include <cstdint>
+#include <stdexcept>
 
 namespace amsim::utils {
   namespace {
@@ -26,5 +27,11 @@ namespace amsim::utils {
       swap_mask = swap_mask ^ (swap_mask >> swap_width);
       bitmatrix_swap(matrix, swap_width, swap_mask);
     }
+  }
+
+  double ddot(std::size_t n, double* x, double* y) {
+    double s = 0.0;
+    for (std::size_t k = 0; k < n; ++k) s += x[k] * y[k];
+    return s;
   }
 }
