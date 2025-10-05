e1ffaf3f894cfbef40c2e8fb3999e32b0298e6a3
Kári Hlynsson <karihlynsson@Modulor-3.local>
2025-10-05 20:56:55 +0000

    starting work on refactoring phenotype values into shared buffer

--- include/amsim/phenotype.h
+++ include/amsim/phenotype.h
@@ -8,6 +8,21 @@
 #include <amsim/genome.h>
 
 namespace amsim {
+  class PhenoBuffer {
+  public:
+    PhenoBuffer(const std::size_t n_ind, const std::size_t n_pheno,
+                const std::size_t n_comps);
+    inline double* ptr_gen(std::size_t id) const noexcept { return ptr_gen_[id]; }
+    inline double* ptr_env(std::size_t id) const noexcept { return ptr_env_[id]; }
+    inline double* ptr_vert(std::size_t id) const noexcept { return ptr_vert_[id]; }
+    
+  private:
+    std::vector<double> buffer_;
+    std::vector<double*> ptr_gen_;
+    std::vector<double*> ptr_env_;
+    std::vector<double*> ptr_vert_;
+  };
+
   class Phenotype {
   public:
 		Phenotype(const std::string name, const std::vector<size_t>& loci,
@@ -17,6 +32,8 @@ namespace amsim {
     inline std::vector<std::size_t> loci() const& noexcept { return loci_; }
     inline double h2() const noexcept { return h2_; }
 
+    // refactor this into a shared buffer which exposes pointers to phenotypes
+    // which are stored as fields
     inline std::vector<double> values_gen() const& noexcept { return values_gen_; }
     inline std::vector<double> values_env() const& noexcept { return values_env_; }
     inline std::vector<double> values() const& noexcept { return values_; }
