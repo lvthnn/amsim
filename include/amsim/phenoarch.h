1ae960e773bb9eedb178b0b29c09d071c49abb8f
Kári <modulor@Hlynurs-Mac-Studio.local>
2025-10-09 18:14:48 +0000

    implementing `init_mask_`, `init_weights_`, and `init_intersect_` for optimisation setup

--- include/amsim/phenoarch.h
+++ include/amsim/phenoarch.h
@@ -13,22 +13,32 @@
 
 namespace amsim {
 
+  enum class ArchOp {
+    ADDITION = 1,
+    DELETION = -1
+  };
+
   class PhenoArch {
   public:
-    PhenoArch(std::size_t n_pheno, std::vector<std::size_t> n_loc,
-              std::size_t n_loc_total, std::vector<double> h2_gen,
+    PhenoArch(std::size_t n_pheno, std::size_t n_loc_total,
+              std::vector<std::size_t> n_loc, std::vector<double> h2_gen,
               std::vector<double> gen_cor, std::vector<double> env_cor,
               const rng::Xoshiro256ss &rng);
     void gen_env(double* ptr_env, std::size_t n_ind);
-    void optim_arch(double eps = 1e-12, std::size_t max_it = 1e5);
+    void optim_arch(double eps = 1e-12, std::size_t max_it = 3);
 
     std::vector<double> env_chol() const noexcept { return env_chol_; }
     std::vector<double> gen_cor() const noexcept { return gen_cor_; }
 
+    std::vector<double> init_weights_() const;
+    std::vector<std::size_t> init_intersect_(const std::vector<std::uint64_t> &mask) const;
+    std::vector<uint64_t> init_mask_();
+    void to_masks_();
+
   private:
     const std::size_t n_pheno_;
-    const std::vector<std::size_t> n_loc_;
     const std::size_t n_loc_tot_;
+    const std::vector<std::size_t> n_loc_;
     const std::vector<double> h2_gen_;
     const std::vector<double> gen_cor_;
 
--- src/phenoarch.cc
+++ src/phenoarch.cc
@@ -4,6 +4,7 @@
 
 #include <cstddef>
 #include <cstdint>
+#include <cmath>
 #include <vector>
 #include <random>
 #include <numeric>
@@ -21,13 +22,13 @@
 #endif
 
 namespace amsim {
-  PhenoArch::PhenoArch(std::size_t n_pheno, std::vector<std::size_t> n_loc,
-                       std::size_t n_loc_total, std::vector<double> h2_gen,
+  PhenoArch::PhenoArch(std::size_t n_pheno, std::size_t n_loc_total,
+                       std::vector<std::size_t> n_loc, std::vector<double> h2_gen,
                        std::vector<double> gen_cor, std::vector<double> env_cor,
                        const rng::Xoshiro256ss &rng)
     : n_pheno_(n_pheno),
-      n_loc_(std::move(n_loc)),
       n_loc_tot_(n_loc_total),
+      n_loc_(std::move(n_loc)),
       h2_gen_(std::move(h2_gen)),
       gen_cor_(std::move(gen_cor)),
       env_chol_(std::move(env_cor)),
@@ -70,42 +71,93 @@ namespace amsim {
     #endif
   }
 
-  void PhenoArch::optim_arch(double eps, std::size_t max_it) {
-    /**
-     * declare a bit-packed uint64_t buffer for activation masks
-     *
-     * while (|E[cors] - cors_input| > eps) && (it < max_it)
-     *   for each column
-     *     for each word in bit-packed mask
-     *       for each bit
-     *         compute the effect of flipping on the cost
-     *         if minimal remove -> update min_remove
-     *         if maximal add -> update max_add
-     *     engage max_add bit and disengage min_remove bit
-     *
-     * convert masks to std::size_t vectors for each phenotype
-     *
-     * allow phenotypes to access the masks
-     */
-    // @TODO: Implement feasibility checking of inputs given constraints
-    const std::size_t n_words = (n_loc_tot_ + 63) / 64;
-    std::vector<uint64_t> loc_mask(n_pheno_ * n_words);
-    std::vector<std::size_t> cnt_pheno(n_pheno_);
+  std::vector<double> PhenoArch::init_weights_() const {
+    std::vector<double> cost_vec((n_pheno_ * (n_pheno_ + 1)) / 2);
+    std::size_t idx = 0;
+    for (std::size_t i = 0; i < n_pheno_; i++) {
+      for (std::size_t j = i; j < n_pheno_; j++) {
+        cost_vec[idx] = gen_cor_[j * n_pheno_ + i] * std::sqrt(n_loc_[i] * n_loc_[j]);
+        idx++;
+      }
+    }
+    return cost_vec;
+  }
 
-    // generate random-state bit-masks for each phenotype using Floyd's sampling
-    // without replacement algorithm
+  std::vector<uint64_t> PhenoArch::init_mask_() {
+    const std::size_t n_words = (n_loc_tot_ + 63) / 64;
+    std::vector<uint64_t> loc_mask(n_words * n_pheno_);
+
     for (std::size_t pheno = 0; pheno < n_pheno_; pheno++) {
       uint64_t* loc_ptr = &loc_mask[pheno * n_words];
+      const std::size_t n_loc_pheno = n_loc_[pheno];
       for (std::size_t r_id = n_loc_tot_ - n_loc_pheno; r_id < n_loc_tot_; r_id++) {
-        std::size_t l_id = rng_unf_.sample(r_id + 1);
-        std::size_t lw = l_id / 64; std::size_t lo = l_id % 64;
-        std::size_t rw = r_id / 64; std::size_t ro = r_id % 64;
+        const std::size_t l_id = rng_unf_.sample(r_id + 1);
+
+        // @TODO: Change this so we write in row-major format (makes optimisation step less computationally heavy)
+        const std::size_t lw = l_id / 64; const std::size_t lo = l_id % 64;
+        const std::size_t rw = r_id / 64; const std::size_t ro = r_id % 64;
+
         if (loc_ptr[lw] & (1ull << lo)) loc_ptr[rw] |= (1ull << ro);
         else loc_ptr[lw] |= (1ull << lo);
       }
     }
-
-    // optimise expected panmictic genetic correlation matrix greedily
-    // (1) prepare the matrix of
+    return loc_mask;
   }
+
+  std::vector<std::size_t> PhenoArch::init_intersect_(const std::vector<uint64_t> &mask) const {
+    std::vector<std::size_t> intersect(n_pheno_ * (n_pheno_ - 1) / 2);
+    const std::size_t n_words = (n_loc_tot_ + 63) / 64;
+
+    std::size_t id = 0;
+    for (std::size_t i = 0; i < n_pheno_; i++) {
+      for (std::size_t j = i + 1; j < n_pheno_; j++) {
+        const uint64_t* pheno_i = &mask[n_words * i];
+        const uint64_t* pheno_j = &mask[n_words * j];
+        for (std::size_t word = 0; word < n_words; word++) {
+          intersect[id] += __builtin_popcountll(pheno_i[word] & pheno_j[word]);
+        }
+        id++;
+      }
+    }
+    return intersect;
+  }
+
+  // void PhenoArch::optim_arch(double eps, std::size_t max_it) {
+  //   const std::size_t n_words = (n_loc_tot_ + 63) / 64;
+  //   std::vector<double> init_weights = init_weights_();
+  //   std::vector<uint64_t> mask = init_mask_();
+
+  //   for (std::size_t it = 0; it < max_it; it++) {
+  //     // index of the phenotype currently inspected
+  //     std::size_t pheno = it % n_pheno_;
+
+  //     // pointer to phenotype locus mask in buffer
+  //     uint64_t *ptr_pheno = &mask[n_words * pheno];
+
+  //     for (std::size_t loc = 0; loc < n_loc_tot_; loc++) {
+  //       // block containing locus in mask
+  //       std::size_t block = loc / 64;
+
+  //       // relative position of locus in the mask
+  //       std::size_t offset = loc % 64;
+
+  //       // what kind of operation are we considering?
+  //       ArchOp op = (ptr_pheno[block] & (1ull << offset))
+  //         ? ArchOp::ADDITION
+  //         : ArchOp::DELETION;
+
+  //       // scan over same locus in the other phenotypes
+  //       for (std::size_t pheno_adj = 0; pheno_adj < n_pheno_; pheno_adj++) {
+  //         if (pheno == pheno_adj) continue;
+
+  //         // pointer to adjacent phenotype locus mask in buffer
+  //         uint64_t *ptr_pheno_adj = &mask[n_words * pheno_adj];
+
+  //         // if the relevant site is activated, increment or decrement the
+  //         // intersection list depending on
+  //       }
+
+  //     }
+  //   }
+  // }
 }
