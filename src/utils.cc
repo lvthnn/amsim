60a78b5ca12108be70574e7d6f30574317a83ba2
lvthnn <kari.hlynsson@gmail.com>
2025-10-10 23:54:27 +0000

    minor changes

--- src/utils.cc
+++ src/utils.cc
@@ -1,8 +1,11 @@
 #include <cstddef>
 #include <cstdint>
+#include <chrono>
+#include <iostream>
 #include <stdexcept>
 
 namespace amsim::utils {
+
   namespace {
     void bitmatrix_swap(std::uint64_t matrix[], std::size_t width,
                         std::uint64_t mask) {
@@ -21,17 +24,11 @@ namespace amsim::utils {
 
   void bitmatrix_transpose(std::uint64_t* matrix) {
     std::size_t swap_width = 64;
-    std::uint64_t swap_mask = ((std::uint64_t)(-1));
+    std::uint64_t swap_mask = static_cast<std::uint64_t>(-1);
     while (swap_width != 1) {
       swap_width >>= 1;
       swap_mask = swap_mask ^ (swap_mask >> swap_width);
       bitmatrix_swap(matrix, swap_width, swap_mask);
     }
-  }
-
-  double ddot(std::size_t n, double* x, double* y) {
-    double s = 0.0;
-    for (std::size_t k = 0; k < n; ++k) s += x[k] * y[k];
-    return s;
   }
 }
