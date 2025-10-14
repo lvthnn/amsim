84a704e1531c0a95a121728f827c2b7780db4e7a
Kári <modulor@Hlynurs-Mac-Studio.local>
2025-10-14 13:16:17 +0000

    small changes

--- src/mating.cc
+++ src/mating.cc
@@ -2,7 +2,6 @@
 #include <vector>
 #include <numeric>
 #include <algorithm>
-#include <stdexcept>
 
 #if defined(__APPLE__) && defined(USE_BLAS)
   #include <Accelerate/Accelerate.h>
@@ -40,27 +39,28 @@ namespace amsim {
   // male_ and female_
   void AssortativeModel::setup_(std::vector<std::size_t> state) {
     for (std::size_t pheno = 0; pheno < n_pheno_; pheno++) {
-      const double* ptr_male_ = ptr_tot_[pheno];
+      // use standardised total components -- i.e. subtract the comp_mean and
+      // comp_var
+      std::vector<double> ones(n_sex_, 1.0);
+
+      const double* ptr_male_   = ptr_tot_[pheno];
       const double* ptr_female_ = ptr_male_ + n_sex_;
-      double* male_col_ = male_.data() + pheno * n_sex_;
-      double* female_col_ = female_.data() + pheno * n_sex_;
+      double* male_col_         = male_.data() + pheno * n_sex_;
+      double* female_col_       = female_.data() + pheno * n_sex_;
 
       std::copy_n(ptr_male_, n_sex_, male_col_);
-
       for (std::size_t ind = 0; ind < n_sex_; ind++)
         female_col_[ind] = ptr_female_[state[ind]];
+
+      // standardise the phenotype values :D
     }
   }
 
   std::vector<double> AssortativeModel::cmp_cor_() {
     std::vector<double> cross_cor_(n_pheno_ * n_pheno_);
-    #if defined(USE_BLAS)
-      cblas_dgemm(CblasColMajor, CblasTrans, CblasNoTrans,
-                  n_sex_, n_sex_, n_pheno_, 1.0 / static_cast<double>(n_pheno_),
-                  male_.data(), 1, female_.data(), 1, 0.0, cross_cor_.data(), 1);
-    #else
-      throw std::runtime_error("Not implemented.");
-    #endif
+    cblas_dgemm(CblasColMajor, CblasTrans, CblasNoTrans,
+                n_sex_, n_sex_, n_pheno_, 1.0 / static_cast<double>(n_pheno_),
+                male_.data(), 1, female_.data(), 1, 0.0, cross_cor_.data(), 1);
     return cross_cor_;
   }
 }
