3a8c9a02896661b466963a55373da8507125648e
Kári <modulor@Hlynurs-Mac-Studio.local>
2025-10-17 00:45:25 +0000

    delegate I/O streaming of `Metric` buffers to `Simulation`

--- include/amsim/metric.h
+++ include/amsim/metric.h
@@ -3,51 +3,64 @@
 
 #pragma once
 #include <string>
-#include <fstream>
-#include <utility>
+#include <optional>
 
 #include <amsim/genome.h>
 #include <amsim/phenotype.h>
 
 namespace amsim {
-  using MetricFunc = std::function<std::vector<double>(std::vector<Phenotype>&, Genome&)>;
+  using MetricFunc = std::function<std::vector<double>(PhenotypeList&, Genome&)>;
 
   class Metric {
   public:
-    Metric(MetricFunc f, const std::string& file, std::string name,
-           const std::size_t n_rows, const std::size_t n_cols = 1);
-    void stream(std::vector<Phenotype>& phenotypes, Genome& genome);
+    Metric(MetricFunc f, const std::string &name,
+           const std::size_t n_rows, const std::size_t n_cols = 1,
+           std::optional<std::vector<std::string>> el_names = std::nullopt);
+
+    inline const std::string name() const noexcept { return name_; }
+    inline const std::size_t n_rows() const noexcept { return n_rows_; }
+    inline const std::size_t n_cols() const noexcept { return n_cols_; }
+    inline const bool named() const noexcept { return named_; }
+    inline const std::vector<std::string> names() const noexcept { return el_names_; }
+    void stream(PhenotypeList &phenotypes, Genome &genome);
 
   private:
     MetricFunc f_;
-    std::size_t it_;
-    std::ofstream file_;
     const std::string name_;
     const std::size_t n_rows_;
     const std::size_t n_cols_;
     std::vector<double> buf_;
+
+    bool named_ = false;
+    std::vector<std::string> el_names_;
+
+    std::size_t it_ = 0;
   };
 
   namespace metrics {
     Metric make_metric(MetricFunc f, const std::string& name,
-                       const std::string& file, const std::size_t n_rows,
-                       const std::size_t n_cols = 1);
+                       const std::size_t n_rows,
+                       const std::size_t n_cols = 1,
+                       std::optional<std::vector<std::string>> el_names = std::nullopt);
 
     namespace genome {
-      inline std::vector<double> floc_maf(std::vector<Phenotype> &phenotypes, Genome &genome) { return genome.v_lmaf(); }
-      inline std::vector<double> floc_mean(std::vector<Phenotype> &phenotypes, Genome &genome) { return genome.v_lmean(); }
-      inline std::vector<double> floc_var(std::vector<Phenotype> &phenotype, Genome &genome) { return genome.v_lvar(); }
+      inline std::vector<double> floc_mean(PhenotypeList &phenotypes, Genome &genome) { return genome.v_lmean(); }
+      inline std::vector<double> floc_var(PhenotypeList &phenotypes, Genome &genome) { return genome.v_lvar(); }
+      inline std::vector<double> floc_maf(PhenotypeList &phenotypes, Genome &genome) { return genome.v_lmaf(); }
     }
 
     namespace phenome {
       MetricFunc fcomp_cor(ComponentType type);
     }
 
-    Metric comp_cor(const std::string& file, const std::size_t n_pheno, ComponentType type);
-    inline auto loc_maf(const std::string& file, const std::size_t n_loc) { return make_metric(genome::floc_maf, "loc_mafs", file, n_loc); }
-    inline auto loc_mean(const std::string& file, const std::size_t n_loc) { return make_metric(genome::floc_mean, "loc_means", file, n_loc); }
-    inline auto loc_var(const std::string& file, const std::size_t n_loc) { return make_metric(genome::floc_var, "loc_vars", file, n_loc); }
+    Metric pheno_h2(const std::size_t n_pheno);
+    Metric comp_cor(const std::size_t n_pheno, ComponentType type);
+    inline Metric loc_maf(const std::size_t n_loc) { return make_metric(genome::floc_maf, "loc_mafs", n_loc); }
+    inline Metric loc_mean(const std::size_t n_loc) { return make_metric(genome::floc_mean, "loc_means", n_loc); }
+    inline Metric loc_var(const std::size_t n_loc) { return make_metric(genome::floc_var, "loc_vars", n_loc); }
   }
+
+  using MetricList = std::vector<std::reference_wrapper<Metric>>;
 }
 
 #endif //AMSIMCPP_METRIC_H
