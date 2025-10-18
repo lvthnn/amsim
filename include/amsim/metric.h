6efdbc06884726ceee33fea4b0b6fe239f3e0ebb
lvthnn <kari.hlynsson@gmail.com>
2025-10-18 13:22:34 +0000

    use `SimulationContext` reference

--- include/amsim/metric.h
+++ include/amsim/metric.h
@@ -5,36 +5,27 @@
 #include <string>
 #include <optional>
 
-#include <amsim/genome.h>
-#include <amsim/phenotype.h>
+#include <amsim/simulation_context.h>
 
 namespace amsim {
-  using MetricFunc = std::function<std::vector<double>(PhenotypeList&, Genome&)>;
-
   class Metric {
   public:
     Metric(MetricFunc f, const std::string &name,
            const std::size_t n_rows, const std::size_t n_cols = 1,
            std::optional<std::vector<std::string>> el_names = std::nullopt);
 
-    inline const std::string name() const noexcept { return name_; }
-    inline const std::size_t n_rows() const noexcept { return n_rows_; }
-    inline const std::size_t n_cols() const noexcept { return n_cols_; }
-    inline const bool named() const noexcept { return named_; }
-    inline const std::vector<std::string> names() const noexcept { return el_names_; }
-    void stream(PhenotypeList &phenotypes, Genome &genome);
+    const std::string name;
+    const std::size_t n_rows;
+    const std::size_t n_cols;
+    const bool named;
+    const std::vector<std::string> names;
+    
+    std::string header();
+    std::string stream(const SimulationContext &ctx);
 
   private:
     MetricFunc f_;
-    const std::string name_;
-    const std::size_t n_rows_;
-    const std::size_t n_cols_;
     std::vector<double> buf_;
-
-    bool named_ = false;
-    std::vector<std::string> el_names_;
-
-    std::size_t it_ = 0;
   };
 
   namespace metrics {
@@ -44,9 +35,17 @@ namespace amsim {
                        std::optional<std::vector<std::string>> el_names = std::nullopt);
 
     namespace genome {
-      inline std::vector<double> floc_mean(PhenotypeList &phenotypes, Genome &genome) { return genome.v_lmean(); }
-      inline std::vector<double> floc_var(PhenotypeList &phenotypes, Genome &genome) { return genome.v_lvar(); }
-      inline std::vector<double> floc_maf(PhenotypeList &phenotypes, Genome &genome) { return genome.v_lmaf(); }
+      inline std::vector<double> floc_mean(const SimulationContext &ctx) {
+        return ctx.genome.v_lmean();
+      }
+
+      inline std::vector<double> floc_var(const SimulationContext &ctx) {
+        return ctx.genome.v_lvar();
+      }
+
+      inline std::vector<double> floc_maf(const SimulationContext &ctx) {
+        return ctx.genome.v_lmaf();
+      }
     }
 
     namespace phenome {
@@ -55,12 +54,19 @@ namespace amsim {
 
     Metric pheno_h2(const std::size_t n_pheno);
     Metric comp_cor(const std::size_t n_pheno, ComponentType type);
-    inline Metric loc_maf(const std::size_t n_loc) { return make_metric(genome::floc_maf, "loc_mafs", n_loc); }
-    inline Metric loc_mean(const std::size_t n_loc) { return make_metric(genome::floc_mean, "loc_means", n_loc); }
-    inline Metric loc_var(const std::size_t n_loc) { return make_metric(genome::floc_var, "loc_vars", n_loc); }
-  }
 
-  using MetricList = std::vector<std::reference_wrapper<Metric>>;
+    inline Metric loc_maf(const std::size_t n_loc) {
+      return make_metric(genome::floc_maf, "loc_mafs", n_loc);
+    }
+
+    inline Metric loc_mean(const std::size_t n_loc) {
+      return make_metric(genome::floc_mean, "loc_means", n_loc);
+    }
+
+    inline Metric loc_var(const std::size_t n_loc) {
+      return make_metric(genome::floc_var, "loc_vars", n_loc);
+    }
+  }
 }
 
 #endif //AMSIMCPP_METRIC_H
