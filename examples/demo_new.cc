d5765da6f6c2f0dbca615de7c60d2f7875cc8f7f
lvthnn <kari.hlynsson@gmail.com>
2025-10-18 13:22:19 +0000

    use builder pattern in demo instead of explicit instantiation

--- examples/demo_new.cc
+++ examples/demo_new.cc
@@ -1,32 +1,33 @@
 #include <cstddef>
 #include <vector>
+#include <iostream>
 
 #include <amsim/simulation_builder.h>
 
 int main() {
-  std::vector<double> v_maf(5000, 0.5);
-  std::vector<double> v_rec(5000, 0.5);
-  std::vector<double> v_mut(5000, 1e-8);
+  std::vector<double> v_maf(8000, 0.5);
+  std::vector<double> v_rec(8000, 0.5);
+  std::vector<double> v_mut(8000, 1e-8);
 
-  amsim::Simulation simulation = amsim::SimulationBuilder()
-    .simulation(15, 256000, "amsim_1610", 123456ull)
-    .genome(5000, v_maf, v_rec, v_mut)
-    .phenome(3,
-             {"height", "weight", "bmi"},
-             {1000, 1000, 1000},
-             {0.5, 0.5, 0.5},
-             {0.5, 0.5, 0.5},
-             {0.0, 0.0, 0.0},
-             {1.0, 0.2, 0.1,
-              0.2, 1.0, 0.1,
-              0.1, 0.1, 1.0},
-             {1.0, 0.2, 0.1,
-              0.2, 1.0, 0.1,
-              0.1, 0.1, 1.0})
-    .mating(amsim::MatingType::ASSORTATIVE, 1e6, 1e-9, 0.9999999,
-            std::vector<double>{0.2, 0.2, 0.2,
-                                0.2, 0.2, 0.2,
-                                0.2, 0.2, 0.2})
-    .metrics(std::vector<amsim::Metric>{ amsim::metrics::pheno_h2("pheno_heritability", 3) })
-    .build();
+  amsim::SimulationBuilder builder;
+
+  builder.simulation(50, 32000, "amsim_1610", 12345671284124888ull)
+         .genome(8000, v_maf, v_rec, v_mut)
+         .phenome(2, {"height", "weight"},
+                     {4000, 4000},
+                     {0.5, 0.5},
+                     {0.5, 0.5},
+                     {0.0, 0.0},
+                     {1.0, 0.0,
+                      0.0, 1.0},
+                     {1.0, 0.0,
+                      0.0, 1.0})
+          .mating(amsim::MatingType::ASSORTATIVE, 2e6, 1e-9, 0.9999999,
+                  std::vector<double>{0.2, 0.2,
+                                      0.2, 0.2})
+          .metrics(std::vector<amsim::Metric>{ amsim::metrics::pheno_h2(2) });
+
+  amsim::Simulation simulation = builder.build();
+  
+  simulation.run();
 }
