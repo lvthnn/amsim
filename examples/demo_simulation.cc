15defc26ee7b2a23265512dc25f31a5afd230f89
Kári <modulor@Hlynurs-Mac-Studio.local>
2025-10-20 02:06:24 +0000

    retuning annealing routine

--- examples/demo_simulation.cc
+++ examples/demo_simulation.cc
@@ -1,6 +1,7 @@
 #include <cstddef>
 #include <vector>
 
+#include <amsim/mating.h>
 #include <amsim/simulation_builder.h>
 
 int main() {
@@ -10,23 +11,24 @@ int main() {
 
   amsim::SimulationBuilder builder;
 
-  builder.simulation(50, 256000, "amsim_1610", 12345671284124888ull)
+  builder.simulation(5, 64000, "amsim_1610", 12345671284124888ull)
          .genome(8000, v_maf, v_rec, v_mut)
-         .phenome(2, {"height", "weight"},
-                     {4000, 4000},
-                     {0.5, 0.5},
-                     {0.5, 0.5},
-                     {0.0, 0.0},
-                     {1.0, 0.0,
-                      0.0, 1.0},
-                     {1.0, 0.0,
-                      0.0, 1.0})
-          .mating(amsim::MatingType::ASSORTATIVE, 2e6, 1e-9, 0.9999999,
-                  std::vector<double>{0.2, 0.2,
-                                      0.2, 0.2})
+         .phenome(2,
+                  {"height", "weight"},
+                  {4000, 4000},
+                  {0.5, 0.5},
+                  {0.5, 0.5},
+                  {0.0, 0.0},
+                  {1.0, 0.0,
+                   0.0, 1.0},
+                  {1.0, 0.0,
+                   0.0, 1.0})
+          .mating(amsim::MatingType::ASSORTATIVE, 2e6, 0.5, 0.9999,
+                  std::vector<double>{0.4, 0.3,  // [ 0.4  0.2
+                                      0.2, 0.5}) //   0.3  0.5 ]
           .metrics(std::vector<amsim::Metric>{ amsim::metrics::pheno_h2(2) });
 
   amsim::Simulation simulation = builder.build();
-  
+
   simulation.run();
 }
