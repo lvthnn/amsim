f06262e58454ca44ddc83d5d1e97a05384918777
Kári <modulor@Hlynurs-Mac-Studio.local>
2025-10-22 14:21:54 +0000

    implement parallel simulation in `amsimpy`

--- python/amsim/simulation.py
+++ python/amsim/simulation.py
@@ -1,14 +1,86 @@
-from pathlib import Path
+"""Implements user interface to amsimcpp backend"""
 from time import time_ns
-from typing import Self, List
+from pathlib import Path
+from typing import Self
+from multiprocessing import Pool
+
+import numpy as np
 
 from amsim._core import _SimulationBuilder, _Simulation, MatingType
-from amsim._core import _Metric
+from amsim.metrics import MetricSpec, build_metrics
 
-from amsim.utils import broadcast_values
+from amsim.utils import broadcast_values, column_major
+
+
+def _run_single_simulation(args: tuple[dict, int]):
+    params, rep_id = args
+
+    from amsim import SimulationBuilder
+
+    output_rep = Path(params['simulation']['output_dir']) / f'rep_{rep_id:03d}'
+    output_rep.mkdir(parents=True, exist_ok=True)
+
+    builder = SimulationBuilder()
+
+    builder.simulation(
+        n_generations=params['simulation']['n_generations'],
+        n_individuals=params['simulation']['n_individuals'],
+        output_dir=output_rep,
+        random_seed=params['simulation']['random_seed']
+    ).genome(
+        n_loci=params['genome']['n_loci'],
+        locus_mafs=params['genome']['locus_mafs'],
+        locus_recombination=params['genome']['locus_recombination'],
+        locus_mutation=params['genome']['locus_mutation']
+    ).phenome(
+        n_phenotypes=params['phenome']['n_phenotypes'],
+        names=params['phenome']['names'],
+        loci=params['phenome']['loci'],
+        h2_genetic=params['phenome']['h2_genetic'],
+        h2_environmental=params['phenome']['h2_environmental'],
+        h2_vertical=params['phenome']['h2_vertical'],
+        genetic_cor=params['phenome']['genetic_cor'],
+        environmental_cor=params['phenome']['environmental_cor']
+    ).mating(
+        mating_type=params['mating']['mating_type'],
+        n_iterations=params['mating']['n_iterations'],
+        temp_init=params['mating']['temp_init'],
+        temp_decay=params['mating']['temp_decay'],
+        mate_cor=params['mating']['mate_cor']
+    ).metrics(
+        metric_specs=params['metrics']['metric_specs']
+    )
+
+    simulation = builder.build()
+    simulation.run()
+    return output_rep
+
+
+class Simulation:
+    def __init__(self, params: dict, simulation: _Simulation):
+        self._params = params
+        self._simulation = simulation
+        self._results = None
+
+    def run(self, n_replicates: int = 1, n_threads: int = 1):
+        if n_replicates == 1:
+            self._simulation.run()
+        else:
+            self._run_replicates(n_replicates, n_threads)
+
+    def _run_replicates(self, n_replicates: int, n_threads: int):
+        output_dirs = []
+        args = [(self._params, rep) for rep in range(n_replicates)]
+
+        with Pool(processes=n_threads) as pool:
+            output_dirs = pool.map(_run_single_simulation, args)
+
+        self._results = output_dirs
+
 
 class SimulationBuilder:
     def __init__(self):
+        self._params = dict()
         self._builder = _SimulationBuilder()
 
     def simulation(
@@ -21,52 +93,119 @@ class SimulationBuilder:
         if random_seed is None:
             random_seed = int(time_ns()) & 0xFFFFFFFFF
 
-        self._builder.simulation(n_generations=n_generations,
-                                 n_individuals=n_individuals,
-                                 output_dir=str(output_dir),
-                                 random_seed=random_seed)
+        Path(output_dir).mkdir(parents=True, exist_ok=True)
+
+        if isinstance(output_dir, Path):
+            output_dir = str(output_dir.resolve())
+
+        self._params['simulation'] = {
+            'n_generations': n_generations,
+            'n_individuals': n_individuals,
+            'output_dir': output_dir,
+            'random_seed': random_seed
+        }
+
+        self._builder.simulation(
+            n_generations=n_generations,
+            n_individuals=n_individuals,
+            output_dir=output_dir,
+            random_seed=random_seed
+        )
+
         return self
 
-    def genome(self, n_loci: int, locus_mafs: float | List[float],
-               locus_recombination: float | List[float],
-               locus_mutation: float | List[float]) -> Self:
-        self._builder.genome(n_loci=n_loci,
-                             locus_mafs=broadcast_values(locus_mafs, n_loci),
-                             locus_recombination=broadcast_values(locus_recombination, n_loci),
-                             locus_mutation=broadcast_values(locus_mutation, n_loci))
+    def genome(
+        self,
+        n_loci: int,
+        locus_mafs: float | list[float],
+        locus_recombination: float | list[float],
+        locus_mutation: float | list[float]
+    ) -> Self:
+        self._params['genome'] = {
+            'n_loci': n_loci,
+            'locus_mafs': broadcast_values(locus_mafs, n_loci),
+            'locus_recombination': broadcast_values(locus_recombination, n_loci),
+            'locus_mutation': broadcast_values(locus_mutation, n_loci)
+        }
+
+        self._builder.genome(
+            n_loci=n_loci,
+            locus_mafs=broadcast_values(locus_mafs, n_loci),
+            locus_recombination=broadcast_values(locus_recombination, n_loci),
+            locus_mutation=broadcast_values(locus_mutation, n_loci)
+        )
         return self
 
-    def phenome(self, n_phenotypes: int, names: list[str], loci: list[int],
-                h2_genetic: list[float], h2_environmental: list[float],
-                h2_vertical: list[float], genetic_cor: list[float],
-                environmental_cor: list[float]) -> Self:
-        self._builder.phenome(n_phenotypes=n_phenotypes,
-                              names=names,
-                              loci=loci,
-                              h2_genetic=h2_genetic,
-                              h2_environmental=h2_environmental,
-                              h2_vertical=h2_vertical,
-                              genetic_cor=genetic_cor,
-                              environmental_cor=environmental_cor)
+    def phenome(
+        self,
+        n_phenotypes: int,
+        names: list[str],
+        loci: int | list[int],
+        h2_genetic: float | list[float],
+        h2_environmental: float | list[float],
+        h2_vertical: float | list[float],
+        genetic_cor: list[list[float]] | np.ndarray,
+        environmental_cor: list[list[float]] | np.ndarray
+    ) -> Self:
+        self._params['phenome'] = {
+            'n_phenotypes': n_phenotypes,
+            'names': names,
+            'loci': broadcast_values(loci, n_phenotypes),
+            'h2_genetic': broadcast_values(h2_genetic, n_phenotypes),
+            'h2_environmental': broadcast_values(h2_environmental, n_phenotypes),
+            'h2_vertical': broadcast_values(h2_vertical, n_phenotypes),
+            'genetic_cor': column_major(genetic_cor),
+            'environmental_cor': column_major(environmental_cor)
+        }    
+
+        self._builder.phenome(
+            n_phenotypes=n_phenotypes,
+            names=names,
+            loci=broadcast_values(loci, n_phenotypes),
+            h2_genetic=broadcast_values(h2_genetic, n_phenotypes),
+            h2_environmental=broadcast_values(h2_environmental, n_phenotypes),
+            h2_vertical=broadcast_values(h2_vertical, n_phenotypes),
+            genetic_cor=column_major(genetic_cor),
+            environmental_cor=column_major(environmental_cor)
+        )
         return self
 
-    def mating(self, mating_type: MatingType, n_iterations: int,
-               temp_init: float, temp_decay: float, mate_cor: list[float]) -> Self:
-        self._builder.mating(mating_type=mating_type,
-                             n_iterations=n_iterations,
-                             temp_init=temp_init,
-                             temp_decay=temp_decay,
-                             mate_cor=mate_cor)
+    def mating(
+        self,
+        mating_type: MatingType,
+        n_iterations: int,
+        temp_init: float,
+        temp_decay: float,
+        mate_cor: list[list[float]] | np.ndarray
+    ) -> Self:
+        self._params['mating'] = {
+            'mating_type': mating_type,
+            'n_iterations': n_iterations,
+            'temp_init': temp_init,
+            'temp_decay': temp_decay,
+            'mate_cor': column_major(mate_cor)
+        }
+
+        self._builder.mating(
+            mating_type=mating_type,
+            n_iterations=n_iterations,
+            temp_init=temp_init,
+            temp_decay=temp_decay,
+            mate_cor=column_major(mate_cor)
+        )
         return self
 
-
-    def metric(self, metrics: list[_Metric]) -> Self:
-        self._builder.metric(metrics=metrics)
+    def metrics(self, metric_specs: list[MetricSpec]) -> Self:
+        # implement pickling support for metrics so we can pass the metrics
+        # like so
+        self._params['metrics'] = {
+            'metric_specs': metric_specs 
+        }
+        metrics = build_metrics(metric_specs)
+        self._builder.metrics(metrics=metrics)
         return self
 
-    def build(self) -> _Simulation:
-        return self._builder.build()
-
-class Simulation:
-    def __init__(self, config):
-        raise NotImplementedError('config initialisation not supported yet')
+    def build(self) -> Simulation:
+        # build should 
+        simulation: _Simulation = self._builder.build()
+        return Simulation(self._params, simulation)
