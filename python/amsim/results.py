e4c346be8a35fd5e49c39c6dbcca93906523a69a
Kári <modulor@Hlynurs-Mac-Studio.local>
2025-10-22 14:22:50 +0000

    start working on `SimulationResults`

--- python/amsim/results.py
+++ python/amsim/results.py
@@ -6,16 +6,6 @@ import pandas as pd
 
 
 class SimulationResults:
-    def __init__(self, result_dir: Path | str) -> None:
-        self.result_dir: Path = Path(result_dir).resolve()
-        self.files: list[Path] = [Path(result_dir, f) for f in listdir(self.result_dir)]
-        self.data: dict[Path, pd.DataFrame] = {}
-
-    def load_results(self):
-        for f in self.files:
-            self.data[f] = pd.read_table(f, sep="\t")
-        print(self.data.values())
-        pass
-
-    def summarise_results(self):
-        pass
+    def __init__(self, output_dir: str | Path) -> None:
+        self._output_dir = Path(output_dir)
+        print(output_dir)
