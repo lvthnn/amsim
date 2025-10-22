83cee1d753887943073ae83b5f21c36fc6c114d9
Kári <modulor@Hlynurs-Mac-Studio.local>
2025-10-22 14:22:20 +0000

    use `MetricSpec` for parallel simulations

--- python/amsim/metrics.py
+++ python/amsim/metrics.py
@@ -1,7 +1,6 @@
 from amsim._core import ComponentType
 
 from amsim._core import (
-    _Metric,
     _loc_maf,
     _loc_mean,
     _loc_var,
@@ -15,7 +14,28 @@ from amsim._core import (
     _pheno_latent_comp_xcor
 ) 
 
-def loc_maf(n_loci: int) -> _Metric:
+_METRIC_REGISTRY = {
+    'loc_maf': _loc_maf,
+    'loc_mean': _loc_mean,
+    'loc_var': _loc_var,
+    'pheno_h2': _pheno_h2,
+    'pheno_comp_mean': _pheno_comp_mean,
+    'pheno_comp_var': _pheno_comp_var,
+    'pheno_comp_cor': _pheno_comp_cor,
+    'pheno_comp_xcor': _pheno_comp_xcor,
+    'pheno_latent_h2': _pheno_latent_h2,
+    'pheno_latent_comp_cor': _pheno_latent_comp_cor,
+    'pheno_latent_comp_xcor': _pheno_latent_comp_xcor
+}
+
+class MetricSpec:
+    def __init__(self, name, args):
+        self._name, self._args = name, args
+
+def build_metrics(specs: list[MetricSpec]):
+    return [_METRIC_REGISTRY[spec._name](*spec._args) for spec in specs]
+
+def loc_maf(n_loci: int) -> MetricSpec:
     """
     Create locus MAF metric.
 
@@ -24,9 +44,9 @@ def loc_maf(n_loci: int) -> _Metric:
     n_loci : int
         Number of simulated loci
     """
-    return _loc_maf(n_loci)
+    return MetricSpec('loc_maf', (n_loci,))
 
-def loc_mean(n_loci: int) -> _Metric:
+def loc_mean(n_loci: int) -> MetricSpec:
     """
     Create locus mean metric.
 
@@ -35,9 +55,9 @@ def loc_mean(n_loci: int) -> _Metric:
     n_loci : int
         Number of simulated loci
     """
-    return _loc_mean(n_loci)
+    return MetricSpec('loc_mean', (n_loci,))
 
-def loc_var(n_loci: int) -> _Metric:
+def loc_var(n_loci: int) -> MetricSpec:
     """
     Create locus variance metric.
 
@@ -46,9 +66,9 @@ def loc_var(n_loci: int) -> _Metric:
     n_loci : int
         Number of simulated loci
     """
-    return _loc_var(n_loci)
+    return MetricSpec('loc_var', (n_loci,))
 
-def pheno_h2(n_phenotypes: int) -> _Metric:
+def pheno_h2(n_phenotypes: int) -> MetricSpec:
     """
     Create phenotype heritability metric.
 
@@ -57,9 +77,9 @@ def pheno_h2(n_phenotypes: int) -> _Metric:
     n_phenotypes : int
         Number of simulated phenotypes
     """
-    return _pheno_h2(n_phenotypes)
+    return MetricSpec("pheno_h2", (n_phenotypes,))
 
-def pheno_comp_mean(n_phenotypes: int, component_type: ComponentType) -> _Metric:
+def pheno_comp_mean(n_phenotypes: int, component_type: ComponentType) -> MetricSpec:
     """
     Create phenotype component mean metric.
 
@@ -70,9 +90,9 @@ def pheno_comp_mean(n_phenotypes: int, component_type: ComponentType) -> _Metric
     component_type : ComponentType
         Phenotype component type
     """
-    return _pheno_comp_mean(n_phenotypes, component_type)
+    return MetricSpec("pheno_comp_mean", (n_phenotypes, component_type))
 
-def pheno_comp_var(n_phenotypes: int, component_type: ComponentType) -> _Metric:
+def pheno_comp_var(n_phenotypes: int, component_type: ComponentType) -> MetricSpec:
     """
     Create phenotype component variance metric.
 
@@ -83,9 +103,9 @@ def pheno_comp_var(n_phenotypes: int, component_type: ComponentType) -> _Metric:
     component_type : ComponentType
         Phenotype component type
     """
-    return _pheno_comp_var(n_phenotypes, component_type)
+    return MetricSpec("pheno_comp_var", (n_phenotypes, component_type))
 
-def pheno_comp_cor(n_phenotypes: int, component_type: ComponentType) -> _Metric:
+def pheno_comp_cor(n_phenotypes: int, component_type: ComponentType) -> MetricSpec:
     """
     Create phenotype component correlation metric.
 
@@ -96,9 +116,9 @@ def pheno_comp_cor(n_phenotypes: int, component_type: ComponentType) -> _Metric:
     component_type : ComponentType
         Phenotype component type
     """
-    return _pheno_comp_cor(n_phenotypes, component_type)
+    return MetricSpec("pheno_comp_cor", (n_phenotypes, component_type))
 
-def pheno_comp_xcor(n_phenotypes: int, component_type: ComponentType) -> _Metric:
+def pheno_comp_xcor(n_phenotypes: int, component_type: ComponentType) -> MetricSpec:
     """
     Create phenotype component cross-correlation metric.
 
@@ -109,9 +129,9 @@ def pheno_comp_xcor(n_phenotypes: int, component_type: ComponentType) -> _Metric
     component_type : ComponentType
         Phenotype component type
     """
-    return _pheno_comp_xcor(n_phenotypes, component_type)
+    return MetricSpec("pheno_comp_xcor", (n_phenotypes, component_type))
 
-def pheno_latent_h2(n_phenotypes: int) -> _Metric:
+def pheno_latent_h2(n_phenotypes: int) -> MetricSpec:
     """
     Create latent phenotype heritability metric.
 
@@ -120,9 +140,9 @@ def pheno_latent_h2(n_phenotypes: int) -> _Metric:
     n_phenotypes : int
         Number of simulated phenotypes
     """
-    return _pheno_latent_h2(n_phenotypes)
+    return MetricSpec("pheno_latent_h2", (n_phenotypes,))
 
-def pheno_latent_comp_cor(n_phenotypes: int, component_type: ComponentType) -> _Metric:
+def pheno_latent_comp_cor(n_phenotypes: int, component_type: ComponentType) -> MetricSpec:
     """
     Create latent phenotype component correlation metric.
 
@@ -133,9 +153,9 @@ def pheno_latent_comp_cor(n_phenotypes: int, component_type: ComponentType) -> _
     component_type : ComponentType
         Phenotype component type
     """
-    return _pheno_latent_comp_cor(n_phenotypes, component_type)
+    return MetricSpec("pheno_latent_comp_cor", (n_phenotypes, component_type))
 
-def pheno_latent_comp_xcor(n_phenotypes: int, component_type: ComponentType) -> _Metric:
+def pheno_latent_comp_xcor(n_phenotypes: int, component_type: ComponentType) -> MetricSpec:
     """
     Create latent phenotype component cross-correlation metric.
     
@@ -146,4 +166,4 @@ def pheno_latent_comp_xcor(n_phenotypes: int, component_type: ComponentType) -> 
     component_type : ComponentType
         Phenotype component type
     """
-    return _pheno_latent_comp_xcor(n_phenotypes, component_type)
+    return MetricSpec("pheno_latent_comp_xcor", (n_phenotypes, component_type))
