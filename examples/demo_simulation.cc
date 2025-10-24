c406abfee42b9f183a15a745fd1c55d673f0059a
lvthnn <kari.hlynsson@gmail.com>
2025-10-24 06:39:25 +0000

    testing new metrics

--- examples/demo_simulation.cc
+++ examples/demo_simulation.cc
@@ -1,10 +1,10 @@
-#include "amsim/metric.h"
 #include <cstddef>
 #include <vector>
 
 #include <amsim/mating.h>
 #include <amsim/simulation_builder.h>
 #include <amsim/component_type.h>
+#include <amsim/metricspec.h>
 
 int main() {
   std::vector<double> v_maf(8000, 0.5);
@@ -13,7 +13,7 @@ int main() {
 
   amsim::SimulationBuilder builder;
 
-  builder.simulation(10, 32000, "amsim_1610", 12345671284124888ull)
+  builder.simulation(10, 32000, "amsim_1610", 1234568ull)
          .genome(8000, v_maf, v_rec, v_mut)
          .phenome(2,
                   {"height", "weight"},
@@ -28,14 +28,14 @@ int main() {
           .mating(amsim::MatingType::ASSORTATIVE, 2e6, 0.5, 0.9999,
                   std::vector<double>{0.4, 0.3,  // [ 0.4  0.2
                                       0.2, 0.5}) //   0.3  0.5 ]
-          .metrics(std::vector<amsim::Metric>{
-            amsim::metrics::pheno_h2(2),
-            amsim::metrics::pheno_comp_cor(2, amsim::ComponentType::GENETIC),
-            amsim::metrics::pheno_comp_cor(2, amsim::ComponentType::ENVIRONMENTAL),
-            amsim::metrics::pheno_comp_xcor(2, amsim::ComponentType::TOTAL),
-            amsim::metrics::pheno_latent_h2(2),
-            amsim::metrics::pheno_latent_comp_cor(2, amsim::ComponentType::GENETIC),
-            amsim::metrics::pheno_latent_comp_xcor(2, amsim::ComponentType::GENETIC)
+          .metrics({
+            amsim::pheno_h2(),
+            amsim::pheno_comp_cor(amsim::ComponentType::GENETIC),
+            amsim::pheno_comp_cor(amsim::ComponentType::ENVIRONMENTAL),
+            amsim::pheno_comp_xcor(amsim::ComponentType::GENETIC),
+            amsim::pheno_latent_h2(),
+            amsim::pheno_latent_comp_cor(amsim::ComponentType::GENETIC),
+            amsim::pheno_latent_comp_xcor(amsim::ComponentType::GENETIC)
           });
 
   amsim::Simulation simulation = builder.build();
