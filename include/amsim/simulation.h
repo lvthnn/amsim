366e1929583fb9b0f83290c12fd67484e7306fc5
lvthnn <kari.hlynsson@gmail.com>
2025-11-01 16:23:19 +0000

    implement `SimulationConfig&` based ctor

--- include/amsim/simulation.h
+++ include/amsim/simulation.h
@@ -7,6 +7,7 @@
 #include <amsim/metricspec.h>
 #include <amsim/phenoarch.h>
 #include <amsim/phenobuf.h>
+#include <amsim/simulation_config.h>
 #include <amsim/simulation_status.h>
 
 #include <filesystem>
@@ -17,6 +18,11 @@ namespace amsim {
 
 class Simulation {
  public:
+  Simulation(
+      const SimulationConfig& config,
+      std::optional<std::filesystem::path> out_dir_ = std::nullopt,
+      std::optional<std::uint64_t> rng_seed_ = std::nullopt);
+
   Simulation(
       std::size_t n_gen,
       std::size_t n_ind,
@@ -39,13 +45,15 @@ class Simulation {
       double tmp_init,
       double tmp_decay,
       std::vector<MetricSpec> specs,
-      bool require_latent,
-      std::optional<std::uint64_t> rep_id = std::nullopt);
+      bool require_latent);
 
   Simulation(Simulation&& other) noexcept
       : n_gen(other.n_gen),
+        n_ind(other.n_ind),
+        n_loc(other.n_loc),
+        n_pheno(other.n_pheno),
+        pheno_names(std::move(other.pheno_names)),
         out_dir(std::move(other.out_dir)),
-        status_(other.status_),
         rng_(std::move(other.rng_)),
         genome_(std::move(other.genome_)),
         arch_(std::move(other.arch_)),
@@ -61,12 +69,16 @@ class Simulation {
   Simulation& operator=(Simulation&&) = delete;
 
   const std::size_t n_gen;
+  const std::size_t n_ind;
+  const std::size_t n_loc;
+  const std::size_t n_pheno;
+  const std::vector<std::string> pheno_names;
+
   const std::filesystem::path out_dir;
 
   void run();
 
  private:
-  SimulationStatus status_;
   rng::Xoshiro256ss rng_;
   Genome genome_;
   PhenoArch arch_;
@@ -79,6 +91,16 @@ class Simulation {
   std::vector<std::unique_ptr<std::ofstream>> streams_;
   void stream_(std::size_t gen);
 };
+
+void run_simulation(
+    const SimulationConfig& config,
+    std::optional<std::size_t> rep_id = std::nullopt
+);
+
+void run_simulations(
+    const SimulationConfig& config,
+    std::size_t n_replicates,
+    std::size_t n_threads);
 
 }  // namespace amsim
 
