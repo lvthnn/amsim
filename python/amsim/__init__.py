823c545cf9fb6a2b47f54351f66e33d50624812b
lvthnn <kari.hlynsson@gmail.com>
2025-11-10 09:58:27 -0500

    refactor bindings into submodules

--- python/amsim/__init__.py
+++
@@ -1,8 +0,0 @@
-from amsim._core import MatingType, ComponentType
-from amsim.simulation import Simulation
-from amsim.results import SimulationResults
-
-from amsim import metrics
-
-__all__ = ['MatingType', 'ComponentType', 'Simulation', 'SimulationResults',
-           'metrics']
--- r/R/amsimr-package.R
+++
@@ -1,4 +0,0 @@
-## usethis namespace: start
-#' @useDynLib amsimr, .registration = TRUE
-## usethis namespace: end
-NULL
