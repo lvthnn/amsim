b803cfd177612a7b37beff1b05fe345a512bc088
lvthnn <kari.hlynsson@gmail.com>
2025-12-15 16:15:28 +0000

    add cross-component correlation metrics

--- include/amsim/metricspec.h
+++ include/amsim/metricspec.h
@@ -64,9 +64,19 @@ MetricSpec pheno_h2();
 /// @param type Component type
 MetricSpec pheno_comp_cor(ComponentType type);
 
+/// @brief Create spec for phenotype cross-component correlation metric
+/// @param type Left component type
+/// @param type Right component type
+MetricSpec pheno_comp_cor(ComponentType type_l, ComponentType type_r);
+
 /// @brief Create spec for phenotype component cross-correlation metric
 /// @param type Component type
 MetricSpec pheno_comp_xcor(ComponentType type);
+
+/// @brief Create spec for phenotype cross-component cross-correlation metric
+/// @param type Left component type
+/// @param type Right component type
+MetricSpec pheno_comp_xcor(ComponentType type_l, ComponentType type_r);
 
 /// @brief Create spec for phenotype component mean metric
 /// @param type Component type
