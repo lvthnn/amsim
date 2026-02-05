1fb81516b56fc73e4aa2d3fea86793263e667e87
lvthnn <kari.hlynsson@gmail.com>
2026-02-05 15:37:43 +0000

    add accessor and modify initialiser

--- src/data/phenome.cc
+++ src/data/phenome.cc
@@ -5,9 +5,12 @@
 namespace amsim::phenome {
 
 PhenoBuf::PhenoBuf(const std::size_t n_ind, const std::size_t n_pheno)
-    : n_ind_(n_ind), n_sex_(n_ind_ / 2), n_pheno_(n_pheno) {
-  data_.resize(4 * n_ind_ * n_pheno_);
-}
+    : n_ind_(n_ind),
+      n_sex_(n_ind_ / 2),
+      n_pheno_(n_pheno),
+      data_(4 * n_ind_ * n_pheno_),
+      comp_mean_(4, n_pheno_),
+      comp_var_(4, n_pheno_) {};
 
 void PhenoBuf::compute_stats() {
   for (ComponentType type :
@@ -15,10 +18,15 @@ void PhenoBuf::compute_stats() {
         ComponentType::ENVIRONMENTAL,
         ComponentType::VERTICAL,
         ComponentType::TOTAL}) {
-    comp_mean_ = (*this)(type).colwise().mean();
-    comp_var_ =
-        ((*this)(type).rowwise() - comp_mean_).array().square().colwise().sum();
-    comp_var_ *= (1.0 / n_ind_);
+    Eigen::Index col_type = static_cast<int>(type);
+    comp_mean_.row(col_type) = (*this)(type).colwise().mean();
+    comp_var_.row(col_type) =
+        ((*this)(type).rowwise() - comp_mean_.row(col_type))
+            .array()
+            .square()
+            .colwise()
+            .sum() /
+        static_cast<double>(n_ind_);
   }
 }
 
