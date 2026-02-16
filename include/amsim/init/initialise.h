0c8265cb960de5a4b0aa2c5286996b1f3c13c8da
lvthnn <kari.hlynsson@gmail.com>
2026-02-16 16:45:47 +0000

    add `init` module for initialiser functionality

---
+++ include/amsim/init/initialise.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <amsim/core/params.h>
+#include <amsim/core/state.h>
+
+namespace amsim {
+
+class HaplotypeGenerator {
+ public:
+  virtual ~HaplotypeGenerator() = default;
+  virtual void generate_haplotypes(GenoBuf& buf) = 0;
+};
+
+// Class to initialise founder population genotypes from unlinked loci
+class HaplotypeGeneratorIID : public HaplotypeGenerator {
+ public:
+  explicit HaplotypeGeneratorIID(const Params& params)
+      : v_maf_(params.geno.v_maf), bw_() {};
+  void generate_haplotypes(GenoBuf& buf) override;
+
+ private:
+  const Eigen::VectorXd& v_maf_;
+  rng::BernoulliWord<16> bw_;
+};
+
+// Class to initialise founder population genotypes with linkage structure
+// class HaploGeneratorLD : public HaplotypeGenerator {
+//  public:
+//   explicit HaploGeneratorLD(const Params& params);
+//   void generate_haplotypes(GenoBuf& buf) override;
+// 
+//  private:
+//   std::vector<double> v_maf_;
+//   std::vector<double> v_rec_;
+//   std::vector<Eigen::MatrixXd> ld_blocks_;
+//   std::vector<std::size_t> n_loc_block_;
+//   std::size_t n_ld_blocks_;
+// };
+
+}  // namespace amsim
---
+++ include/amsim/init/setup.h
@@ -0,0 +1,70 @@
+#pragma once
+
+#include <amsim/core/log.h>
+#include <amsim/core/params.h>
+#include <amsim/estimate/population.h>
+#include <amsim/sample/sampler.h>
+
+#include <Eigen/Dense>
+#include <cstddef>
+#include <string>
+#include <vector>
+
+namespace amsim {
+
+struct Genome {
+  std::size_t n_loci;
+
+  std::variant<double, Eigen::VectorXd> v_rec = 0.5;
+  std::variant<double, Eigen::VectorXd> v_maf = 0.5;
+  std::variant<double, Eigen::VectorXd> v_mut = 0.0;
+};
+
+struct Phenotype { std::string name; std::size_t n_causal_loci;
+  std::unordered_map<std::string, std::size_t> ids;
+
+  std::optional<Eigen::VectorXd> effects;
+  std::optional<std::vector<std::size_t>> causal_loci;
+
+  double h2_genetic = 0.5;
+  double h2_environmental = 0.5;
+  double h2_nurture = 0.0;
+
+  double nurture_paternal_ratio = 0.5;
+  double nurture_environmental_ratio = 0.5;
+};
+
+struct Mating {
+  Eigen::MatrixXd mate_cor;
+
+  double tolerance = 1e-7;
+  std::size_t max_iterations = 2000000;
+  double initial_temperature = 1.0;
+  double temperature_decay = 0.99;
+};
+
+struct Simulation {
+  std::size_t n_individuals;
+
+  Genome genome;
+  std::vector<Phenotype> phenotypes;
+  std::optional<Eigen::MatrixXd> genetic_component_cor;
+  std::optional<Eigen::MatrixXd> environmental_component_cor;
+
+  Mating mating;
+
+  std::vector<PopulationEstimator> estimators; // population-wide estimators
+  std::vector<SampleSpec> samples; // subpopulation estimators
+
+  std::size_t n_replicates = 1;
+  std::size_t n_threads = 1;
+  std::filesystem::path output_dir;
+  std::optional<std::uint64_t> random_seed;
+
+  LogLevel log_level;
+  bool log_file = false;
+};
+
+Params build_params(const Simulation& simulation);
+
+}  // namespace amsim
