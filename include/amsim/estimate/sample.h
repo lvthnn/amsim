30bd4e61c7df8e3c37d5afb85f89d94c34acd7bd
lvthnn <kari.hlynsson@gmail.com>
2026-02-18 18:03:11 +0000

    add some basic sample estimators

--- include/amsim/estimate/sample.h
+++ include/amsim/estimate/sample.h
@@ -66,10 +66,14 @@ class SampleEstimatorStrategy {
 };
 
 template <Proband P>
-using SampleEstimator = std::function<std::unique_ptr<SampleEstimatorStrategy<P>>(
-        const Params& params, const std::filesystem::path& sample_dir)>;
+using SampleEstimator =
+    std::function<std::unique_ptr<SampleEstimatorStrategy<P>>(
+        const Params& params,
+        std::size_t n_probands,
+        const std::filesystem::path& sample_dir)>;
 
-template <Proband P> using SampleEstimators = std::vector<SampleEstimator<P>>;
+template <Proband P>
+using SampleEstimators = std::vector<SampleEstimator<P>>;
 
 template <Proband P>
 class SampleMean : public SampleEstimatorStrategy<P> {
@@ -90,20 +94,120 @@ class SampleMean : public SampleEstimatorStrategy<P> {
 };
 
 template <Proband P>
+class SampleVar : public SampleEstimatorStrategy<P> {
+ public:
+  explicit SampleVar(
+      const Params& params, const std::filesystem::path& sample_dir)
+      : SampleEstimatorStrategy<P>(
+            sample_dir,
+            "sample_var",
+            params.pheno.names,
+            params.pheno.n_pheno) {}
+
+  void compute(
+      const Eigen::MatrixXd& phenotypes,
+      const Eigen::MatrixXd& /*genotypes*/) override {
+    this->data_ = (phenotypes.rowwise() - phenotypes.colwise().mean())
+                      .array()
+                      .square()
+                      .colwise()
+                      .sum() /
+                  (phenotypes.rows() - 1);
+  }
+};
+
+template <Proband P>
+class SampleMateCor : public SampleEstimatorStrategy<P> {
+ public:
+  explicit SampleMateCor(
+      const Params& params,
+      const std::filesystem::path& sample_dir,
+      std::size_t n_probands)
+      : SampleEstimatorStrategy<P>(
+            sample_dir,
+            "sample_mate_cor",
+            utils::label_matrix(
+                params.pheno.names, params.pheno.names, "_male", "_female"),
+            params.pheno.n_pheno,
+            params.pheno.n_pheno),
+        n_probands_(n_probands),
+        n_pheno_(params.pheno.n_pheno),
+        std_male_(
+            (n_probands_ * ProbandData<P>::ProbandSize) / 2,
+            n_pheno_),
+        std_female_(
+            (n_probands_ * ProbandData<P>::ProbandSize) / 2,
+            n_pheno_) {
+    if constexpr (P == Proband::Individual) {
+      throw std::runtime_error(
+          "Unsupported proband type Proband::Individual for estimator "
+          "SampleMateCor");
+    }
+  }
+
+  // compute centres and standard deviations across columns
+  void compute(
+      const Eigen::MatrixXd& phenotypes,
+      const Eigen::MatrixXd& /*genotypes*/) override {
+    const std::size_t n_pairs = n_probands_ * 3;
+    const std::size_t outer =
+        n_probands_ * ProbandData<P>::ProbandSize;
+
+    using MateMat = Eigen::Map<
+        const Eigen::MatrixXd,
+        Eigen::Unaligned,
+        Eigen::Stride<Eigen::Dynamic, 2>>;
+
+    MateMat males(
+        phenotypes.data(),
+        n_pairs,
+        n_pheno_,
+        {static_cast<Eigen::Index>(outer), 2});
+
+    MateMat females(
+        phenotypes.data() + 1,
+        n_pairs,
+        n_pheno_,
+        {static_cast<Eigen::Index>(outer), 2});
+
+    std_male_ = utils::standardise(males, false);
+    std_female_ = utils::standardise(females, false);
+
+    this->data_ = (1.0 / (n_pairs - 1)) * std_male_.transpose() * std_female_;
+  }
+
+ private:
+  std::size_t n_probands_;
+  std::size_t n_pheno_;
+  Eigen::MatrixXd std_male_;
+  Eigen::MatrixXd std_female_;
+};
+
+template <Proband P>
 inline SampleEstimator<P> SampleMeanEstimator() {
-  return [](const Params& params, const std::filesystem::path& sample_dir) {
+  return [](const Params& params,
+            std::size_t /*n_probands*/,
+            const std::filesystem::path& sample_dir) {
     return std::make_unique<SampleMean<P>>(params, sample_dir);
   };
 }
 
 template <Proband P>
 inline SampleEstimator<P> SampleVarEstimator() {
-
+  return [](const Params& params,
+            std::size_t /*n_probands*/,
+            const std::filesystem::path& sample_dir) {
+    return std::make_unique<SampleVar<P>>(params, sample_dir);
+  };
 }
 
 template <Proband P>
-inline SampleEstimator<P> SampleCorEstimator() {
-
+inline SampleEstimator<P> SampleMateCorEstimator() {
+  return [](const Params& params,
+            std::size_t n_probands,
+            const std::filesystem::path& sample_dir) {
+    return std::make_unique<SampleMateCor<P>>(params, sample_dir, n_probands);
+  };
 }
 
-} // namespace amsim
+}  // namespace amsim
